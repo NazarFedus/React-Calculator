a19e8ee20093cbb01a704f7c3024d5b7fdbde4bb
NazarFedus <simpleprograming7@gmail.com>
2023-02-03 11:22:22 +0000

    add logic

--- src/App.js
+++ src/App.js
@@ -9,29 +9,51 @@ function App() {
   const inputRef = useRef(null);
   const resultRef = useRef(null);
   const [result, setResult] = useState(0);
+  const [currentValue, setCurrentValue] = useState(0);
+
+  const [manipulation, setManipulation] = useState('');
 
   useEffect(() => {
     inputRef.current.value = '';
-  }, [result])
+    inputRef.current.focus();
+  }, [manipulation])
 
   function plus(e) {
     e.preventDefault();
-    setResult((result) => result + Number(inputRef.current.value));
+    setCurrentValue(Number(inputRef.current.value));
+    setManipulation('plus')
   };
 
   function minus(e) {
     e.preventDefault();
-    setResult((result) => result - Number(inputRef.current.value));
-    inputRef.current.value = "";
+    setCurrentValue(Number(inputRef.current.value));
+    setManipulation('minus')
   };
 
   function times(e) {
-    // Add the code for the plus function
+    e.preventDefault();
+    setCurrentValue(Number(inputRef.current.value));
+    setManipulation('multiply')
   };
 
   function divide(e) {
-    // Add the code for the divide function
+    e.preventDefault();
+    setCurrentValue(Number(inputRef.current.value));
+    setManipulation('devide')
   };
+
+  function equel(e) {
+    e.preventDefault();
+    if(manipulation === 'plus') {
+      setResult(currentValue + Number(inputRef.current.value));
+    } else if(manipulation === 'minus') {
+      setResult(currentValue - Number(inputRef.current.value));
+    } else if(manipulation === 'multiply') {
+      setResult(currentValue * Number(inputRef.current.value));
+    } else if(manipulation === 'devide') {
+      setResult(currentValue / Number(inputRef.current.value));
+    }
+  }
 
   function resetInput(e) {
     e.preventDefault();
@@ -52,6 +74,7 @@ function App() {
         <p ref={resultRef}>
           Result: {result}
         </p>
+        <p>Manipulation: {manipulation}</p>
         <input
           pattern="[0-9]"
           ref={inputRef}
@@ -62,6 +85,7 @@ function App() {
         <button onClick={minus}>subtract</button>
         <button onClick={times}>multiply</button>
         <button onClick={divide}>divide</button>
+        <button onClick={equel}>=</button>
 
         <button onClick={resetInput}>resetInput</button>
         <button onClick={resetResult}>resetResult</button>
