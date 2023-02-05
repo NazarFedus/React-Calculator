0fec516ffabc337b7214478015117fb76946e3ca
NazarFedus <simpleprograming7@gmail.com>
2023-02-05 18:13:48 +0000

    add new buttons

--- src/App.js
+++ src/App.js
@@ -18,15 +18,13 @@ function App() {
     inputRef.current.focus();
   }, [manipulation])
 
-  useEffect(() => {
-    setCurrentValue(result)
-    inputRef.current.value = result;
-
-  }, [result])
-
   function clear(){
     setManipulation('')
     inputRef.current.value = '';
+  }
+
+  function btnHandler(e) {
+    inputRef.current.value = e.target.value;
   }
 
   function plus(e) {
@@ -53,7 +51,7 @@ function App() {
     setManipulation('devide')
   };
 
-  function equal(e) {
+  function equel(e) {
     e.preventDefault();
     if(manipulation === 'plus') {
       setResult(currentValue + Number(inputRef.current.value));
@@ -69,14 +67,22 @@ function App() {
 
   function resetInput(e) {
     e.preventDefault();
-    clear();
+    inputRef.current.value = "";
+    setManipulation('');
   };
 
   function resetResult(e) {
     e.preventDefault();
     setResult(0);
-    clear();
+    inputRef.current.value = "";
+    setManipulation('');
   };
+
+  function clickHandler(e) {
+    e.preventDefault();
+    // inputRef.current.value += ;
+    console.log(e.target.value);
+  }
 
   return (
     <div className="App">
@@ -93,13 +99,26 @@ function App() {
           ref={inputRef}
           type="number"
           placeholder="Type a number"
-
         />
-        <button onClick={plus}>add</button>
-        <button onClick={minus}>subtract</button>
-        <button onClick={times}>multiply</button>
-        <button onClick={divide}>divide</button>
-        <button onClick={equal}>=</button>
+        <div>
+        <button onClick={clickHandler} value="1">1</button>
+        <button onClick={clickHandler} value="2">2</button>
+        <button onClick={clickHandler} value="3">3</button>
+        <button onClick={clickHandler} value="4">4</button>
+        </div>
+        <div>
+        <button onClick={clickHandler} value="5">5</button>
+        <button onClick={clickHandler} value="6">6</button>
+        <button onClick={clickHandler} value="7">7</button>
+        <button onClick={clickHandler} value="8">8</button>
+        </div>
+        <button onClick={clickHandler} value="9">9</button>
+        <button onClick={clickHandler} value="0">0</button>
+        <button onClick={plus}>+</button>
+        <button onClick={minus}>-</button>
+        <button onClick={times}>*</button>
+        <button onClick={divide}>/</button>
+        <button onClick={equel}>=</button>
 
         <button onClick={resetInput}>resetInput</button>
         <button onClick={resetResult}>resetResult</button>
