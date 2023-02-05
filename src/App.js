6ffd333b9059398158da17c9795a46a6c6b4cbaa
NazarFedus <simpleprograming7@gmail.com>
2023-02-05 18:43:59 +0000

    add logic to buttons

--- src/App.js
+++ src/App.js
@@ -14,41 +14,41 @@ function App() {
   const [manipulation, setManipulation] = useState('');
 
   useEffect(() => {
-    inputRef.current.value = '';
-    inputRef.current.focus();
-  }, [manipulation])
+    inputRef.current.value = result;
+  }, [result, inputRef])
+
 
   function clear(){
     setManipulation('')
     inputRef.current.value = '';
   }
 
-  function btnHandler(e) {
-    inputRef.current.value = e.target.value;
-  }
-
   function plus(e) {
     e.preventDefault();
     setCurrentValue(Number(inputRef.current.value));
     setManipulation('plus')
+    inputRef.current.value = '';
   };
 
   function minus(e) {
     e.preventDefault();
     setCurrentValue(Number(inputRef.current.value));
     setManipulation('minus')
+    inputRef.current.value = '';
   };
 
   function times(e) {
     e.preventDefault();
     setCurrentValue(Number(inputRef.current.value));
     setManipulation('multiply')
+    inputRef.current.value = '';
   };
 
   function divide(e) {
     e.preventDefault();
     setCurrentValue(Number(inputRef.current.value));
     setManipulation('devide')
+    inputRef.current.value = '';
   };
 
   function equel(e) {
@@ -62,26 +62,25 @@ function App() {
     } else if(manipulation === 'devide') {
       setResult(currentValue / Number(inputRef.current.value));
     }
-    clear();
   }
 
   function resetInput(e) {
     e.preventDefault();
-    inputRef.current.value = "";
-    setManipulation('');
+    clear();
   };
 
   function resetResult(e) {
     e.preventDefault();
     setResult(0);
-    inputRef.current.value = "";
-    setManipulation('');
+    clear();
   };
 
   function clickHandler(e) {
     e.preventDefault();
-    // inputRef.current.value += ;
-    console.log(e.target.value);
+    if(inputRef.current.value === '0') {
+      inputRef.current.value = '';
+    }
+    inputRef.current.value += e.target.value;
   }
 
   return (
@@ -100,28 +99,33 @@ function App() {
           type="number"
           placeholder="Type a number"
         />
-        <div>
-        <button onClick={clickHandler} value="1">1</button>
-        <button onClick={clickHandler} value="2">2</button>
-        <button onClick={clickHandler} value="3">3</button>
-        <button onClick={clickHandler} value="4">4</button>
+        <div className="container">
+          <div>
+            <button onClick={clickHandler} value="1">1</button>
+            <button onClick={clickHandler} value="2">2</button>
+            <button onClick={clickHandler} value="3">3</button>
+            <button onClick={clickHandler} value="4">4</button>
+            <button onClick={clickHandler} value="5">5</button>
+          </div>
+          <div>
+            <button onClick={clickHandler} value="6">6</button>
+            <button onClick={clickHandler} value="7">7</button>
+            <button onClick={clickHandler} value="8">8</button>
+            <button onClick={clickHandler} value="9">9</button>
+            <button onClick={clickHandler} value="0">0</button>
+          </div>
+          <div>
+            <button onClick={plus} className="manipulations">+</button>
+            <button onClick={minus} className="manipulations">-</button>
+            <button onClick={times} className="manipulations">*</button>
+            <button onClick={divide} className="manipulations">/</button>
+            <button onClick={equel} className="manipulations">=</button>
+          </div>
+          <div style={{display: "flex", flexDirection: 'row'}}>
+            <button onClick={resetInput}>resetInput</button>
+            <button onClick={resetResult}>resetResult</button>
+          </div>
         </div>
-        <div>
-        <button onClick={clickHandler} value="5">5</button>
-        <button onClick={clickHandler} value="6">6</button>
-        <button onClick={clickHandler} value="7">7</button>
-        <button onClick={clickHandler} value="8">8</button>
-        </div>
-        <button onClick={clickHandler} value="9">9</button>
-        <button onClick={clickHandler} value="0">0</button>
-        <button onClick={plus}>+</button>
-        <button onClick={minus}>-</button>
-        <button onClick={times}>*</button>
-        <button onClick={divide}>/</button>
-        <button onClick={equel}>=</button>
-
-        <button onClick={resetInput}>resetInput</button>
-        <button onClick={resetResult}>resetResult</button>
       </form>
     </div>
   );
